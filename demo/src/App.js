eb634fd1974a58c6d48ace0a10f1333869980a7b
ndmchuong <minhchuong24052001@gmail.com>
2022-10-13 09:39:33 +0700

    demo jsx render array

--- demo/src/App.js
+++ demo/src/App.js
@@ -2,6 +2,7 @@
 import { Route, Routes, Link } from "react-router-dom";
 import { LogIn } from "./components/LogIn";
 import Nameform, { FormFunction } from "./components/NameForm";
+import { RenderArrayProducts } from "./components/RenderArrayProducts";
 import { About } from "./components/Router/About";
 import { Contact } from "./components/Router/Contact";
 import { Home } from "./components/Router/Home";
@@ -11,34 +12,37 @@ import { NotFound } from "./components/Router/NotFound";
 function App() {
   
   return (
-    // <div className="App">
-    //   <LogIn IsLogin = {true} note = "khi gia tri la true"/>
-    //   <LogIn IsLogin = {false} note = "khi gia tri la false"/>
-    //   <FormFunction/>
-    // </div>
-    <>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/Contact">Contact</Link>
-          </li>
-          <li>
-            <Link to="/About">About</Link>
-          </li>
-        </ul>
-      </nav>
+    <div className="App">
+      {/* <LogIn IsLogin = {true} note = "khi gia tri la true"/>
+      <LogIn IsLogin = {false} note = "khi gia tri la false"/>
+      <FormFunction/> */}
+      <RenderArrayProducts/>
+    </div>
+
+
+    // <>
+    //   <nav>
+    //     <ul>
+    //       <li>
+    //         <Link to="/">Home</Link>
+    //       </li>
+    //       <li>
+    //         <Link to="/Contact">Contact</Link>
+    //       </li>
+    //       <li>
+    //         <Link to="/About">About</Link>
+    //       </li>
+    //     </ul>
+    //   </nav>
     
-    <Routes>
-      <Route path="/" element={<Home/>} />
-      <Route path="/Contact" element={<Contact/>} />
-      <Route path="/Contact/:id" element={<Contact/>}/>
-      <Route path="/About" element={<About/>} />
-      <Route path="*" element={<NotFound/>} />
-    </Routes>
-    </>
+    // <Routes>
+    //   <Route path="/" element={<Home/>} />
+    //   <Route path="/Contact" element={<Contact/>} />
+    //   <Route path="/Contact/:id" element={<Contact/>}/>
+    //   <Route path="/About" element={<About/>} />
+    //   <Route path="*" element={<NotFound/>} />
+    // </Routes>
+    // </>
   );
 }
 
